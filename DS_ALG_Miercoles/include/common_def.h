a33a02beef598a7257433a5858161e5cede5b6bb
cmazcu <carlosmazcu@gmail.com>
2023-11-10 15:00:36 +0100

    add old version

---
+++ DS_ALG_Miercoles/include/common_def.h
@@ -0,0 +1,26 @@
+// common_def.h
+// Escuela Superior de Arte y Tecnologia
+// Algoritmos & Inteligencia Artificial
+// ESAT 2020-2021
+//
+#ifndef __COMMON_DEF_H__
+#define __COMMON_DEF_H__ 1
+
+#define VERBOSE_
+
+typedef enum {
+  kErrorCode_Ok = 0,
+  kErrorCode_Null = -1,
+  kErrorCode_Memory = -2,
+  kErrorCode_NodeNull = -10, 
+  kErrorCode_SrcNull = -11,
+  kErrorCode_BytesZero = -12,
+  kErrorCode_DataNull = -13,
+  kErrorCode_SizeZero = -14,
+  kErrorCode_SizeMismatch = -15,
+  kErrorCode_OpsNull = -16,
+  
+  
+} ErrorCode;
+
+#endif // __COMMON_DEF_H__
