c315fecf89af75b9272fd471e2552e7f8e97d1f4
ALopezESAT <146739217+ALopezESAT@users.noreply.github.com>
2024-03-07 13:56:19 +0100

    Update adt_list.c

--- DS_ALG/src/adt_list.c
+++ DS_ALG/src/adt_list.c
@@ -65,6 +65,8 @@ struct list_ops_s list_ops = { .next = LIST_next,
 
 List* LIST_create(u16 capacity)
 {
+    // ALopezESAT: u16 will never be negative
+    // Also, apply feedback from Movable Head Vector
     if(0 >= capacity)
     {
         return NULL;
@@ -84,6 +86,7 @@ List* LIST_create(u16 capacity)
 
 }
 
+// ALopezESAT: This should be in memory node
 MemoryNode* LIST_next(MemoryNode *node)
 {
     if(NULL == node)
@@ -93,7 +96,7 @@ MemoryNode* LIST_next(MemoryNode *node)
     }
     return node->next_;
 }
-
+// ALopezESAT: same as above
 s16 LIST_setNext(MemoryNode *node, MemoryNode *next)
 {
     if(NULL == node || NULL == next){
@@ -110,11 +113,13 @@ s16 LIST_destroy(List *list) {
     while(NULL != aux)
     {
         // aux->ops_->free(aux->data_);
-        aux->ops_->free(aux);
+        aux->ops_->free(aux); // ALopezESAT: Does this free data?
         aux = aux_next;
         aux_next = aux->next_;
     }
-
+    
+    // ALopezESAT: No need to free head and tail, you have already gone through them previously
+    // Also, mind the tabs!
     MM->free(list->head_);
     MM->free(list->tail_);
     MM->free(list);
@@ -123,6 +128,8 @@ s16 LIST_destroy(List *list) {
 
 s16 LIST_softReset(List *list)
 {
+    // ALopezESAT: You mean NULL == list?
+    // Also... tabs!
   if (list == NULL)
     {
         return kErrorCode_ListNull;
@@ -134,14 +141,15 @@ s16 LIST_softReset(List *list)
 
     while (current_node != NULL)
     {
+        // ALopezESAT: If you are only going to use it in the while, declare it here as well.
         next_node = current_node->next_;
-        free(current_node);
+        free(current_node); // ALopezESAT: Mmmm... just "free"? does not sound like the free we use here...
         current_node = next_node;
     }
     list->head_ = NULL;
     list->tail_ = NULL;
     list->length_ = 0;
-    list->capacity_ = 0;
+    list->capacity_ = 0; // ALopezESAT: Mmm... capacity as well? You don't have it in the reset...
 
     return kErrorCode_Ok;
 
@@ -161,9 +169,11 @@ s16 LIST_reset(List *list)
 
     while (current_node != NULL)
     {
+        // ALopezESAT: Same as above
         next_node = current_node->next_;
+        // ALopezESAT: Ok, you HAVE to make your mind about what free you use! I like this one more BTW...
         MM->free(current_node->data_);
-        current_node->data_ = NULL;
+        current_node->data_ = NULL; // ALopezESAT: No need to set it to null before freeing it.
         MM->free(current_node);
         current_node = next_node;
     }
@@ -180,19 +190,22 @@ s16 LIST_resize(List *list, u16 new_capacity)
     {
         return kErrorCode_ListNull;
     }
-    
+    // ALopezESAT: I would check for lenght instead, as you might have a list with 10 capacity and 2 length and if you
+    // resize to 5 capacity you will not need to do anything else.
     if(list->capacity_ <= new_capacity)
     {
         list->capacity_ = new_capacity;
         return kErrorCode_Ok;
     }
 
+    // ALopezESAT: Redundant check, we already know this.
     if(list->capacity_ > new_capacity)
     {
 
         MemoryNode *current_node = list->head_;
         MemoryNode *next_node;
 
+        // ALopezESAT: I think you might be deleting the whole list here, instead of removing a few elements.
         while (current_node != NULL)
         {
             next_node = current_node->next_;
@@ -217,6 +230,7 @@ s16 LIST_resize(List *list, u16 new_capacity)
 
 u16 LIST_capacity(List *list)
 {
+    // ALopezESAT: "NULL == list" makes you look smarter
     if(list == NULL){
         return 0;
     }
@@ -250,6 +264,9 @@ boolean LIST_isFull(List *list)
     {
         return False;
     }
+    // ALopezESAT: I think capacity should never be 0, so the second check that you have is a problem not solved
+    // somewhere else
+    // Also, for readability, and to reduce code, you could do something like "return list->length_ == list->capacity_"
     if(list->length_ != list->capacity_ || list->length_ == 0){
         return False;
     }
@@ -262,6 +279,8 @@ void* LIST_first(List *list)
     {
         return NULL;
     }
+    // ALopezESAT: Also known as "isEmpty".
+    // This change is not necessary but interesting to think about.
     if(list->head_ == NULL)
     {
         return NULL;
@@ -273,6 +292,7 @@ void* LIST_last(List *list)
 {
     if(list == NULL)
     {
+        // ALopezESAT: You mean... NULL?
         return kErrorCode_ListNull;
     }
     if(list->tail_ == NULL)
@@ -287,12 +307,15 @@ void* LIST_at(List *list, u16 index)
 {
     if(list == NULL)
     {
+        // ALopezESAT: You mean... NULL?
         return kErrorCode_ListNull;
     }
     MemoryNode *aux = list->head_;
-    for(int i = 0; i < index; i++)
+    for(int i = 0; i < index; i++) // ALopezESAT: You mean... ++i?
     {
         aux = aux->next_;
+        // ALopezESAT: Breaking this way out of a for might mean you can check something before it and not go in at
+        // all... like if you should just return last.
         if(i >= list->length_)
         {
             return aux;
@@ -315,6 +338,7 @@ s16 LIST_insertFirst(List *list, void *data, u16 size)
     }
     node->size_ = size;
     node->data_ = data;
+    // ALopezESAT: I would do this check before creating a Memory Node
     if(LIST_isFull(list))
     {
         return kErrorCode_NotEnoughCapacity;
@@ -322,6 +346,7 @@ s16 LIST_insertFirst(List *list, void *data, u16 size)
     //check if the list is empty
     if (LIST_isEmpty(list))
     {
+        // ALopezESAT: I think the node creation already sets next to NULL
         node->next_ = NULL;
         list->head_ = node;
         list->tail_ = node;
@@ -330,6 +355,8 @@ s16 LIST_insertFirst(List *list, void *data, u16 size)
     else
     {
         //check if have enought capacity
+        // ALopezESAT: Why? Or is this just a wrong comment left here for my chance at a large texts that basically
+        // says nothing except for "please, remove the comment above if it is not explaining the code correctly"?
         node->next_ = list->head_;
         list->head_ = node;
         list->length_++;
@@ -350,6 +377,7 @@ s16 LIST_insertLast(List* list, void* data, u16 size)
     }
     node->size_ = size;
     node->data_ = data;
+    // ALopezESAT: Same as above, also, you can use isFull as above
     if (list->length_ >= list->capacity_)
     {
         return kErrorCode_NotEnoughCapacity;
@@ -395,6 +423,7 @@ s16 LIST_insertAt(List *list, void* data, u16 size, u16 index)
     }
 
     // check if is full
+    // ALopezESAT: Same as above
     if (LIST_isFull(list))
     {
         return kErrorCode_NotEnoughCapacity;
@@ -451,11 +480,14 @@ s16 LIST_extractFirst(List *list)
 
     list->length_--;
 
+    // ALopezESAT: I think extract returns data, so instead of freeing it, you should store it and return it
+    // In any case kErrorCode_Ok should be used instead of 0 if you are returning an error code.
     return 0;
 }
 
 s16 LIST_extractLast(List *list)
 {
+    // ALopezESAT: Same feedback as extractFirst
      if (list == NULL)
     {
         return kErrorCode_ListNull;
@@ -523,6 +555,7 @@ s16 LIST_extractAt(List *list, u16 index)
        LIST_extractFirst(list);
        return kErrorCode_Ok;
     }
+    // ALopezESAT: No need for an else here
     else
     {
         // search prev node to extract
@@ -540,11 +573,14 @@ s16 LIST_extractAt(List *list, u16 index)
         node_to_extract = current_node;
 
         // if extract last
+        // ALopezESAT: A bit too late, you might be able to check something like this before going through the whole
+        // list...
         if (index == list->length_ - 1)
         {
             LIST_extractLast(list);
             return kErrorCode_Ok;
         }
+        // ALopezESAT: No need for an else here
         else
         {
             prev_node->next_ = current_node->next_;
@@ -572,6 +608,7 @@ s16 LIST_concat(List *list, List *next_list)
         return kErrorCode_Ok;
     }
     // check if list is empty and copy each other
+    // ALopezESAT: You need to make a copy of the second list, this will not do it.
     if (LIST_isEmpty(list))
     {
         list->head_ = next_list->head_;
@@ -587,6 +624,8 @@ s16 LIST_concat(List *list, List *next_list)
     }
 
     // freeeeeeee memory uwu
+    // ALopezESAT: uwu? really... UWU???? What is that... I don't know what it is... never seen it in my life...
+    // But also... the second list has to stay as it started, so you don't need to freeeeeeeee the memory.
     free(next_list);
 
     return kErrorCode_Ok;
@@ -594,6 +633,7 @@ s16 LIST_concat(List *list, List *next_list)
 
 s16 LIST_traverse(List *list, void(*callback)(MemoryNode*))
 {
+    // ALopezESAT: Ok... I'll say it one last time... "NULL == list" PLEASEANDTHANKYOU!!!!
     if (list == NULL)
     {
         return kErrorCode_ListNull;
